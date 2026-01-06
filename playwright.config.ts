24713cf9e2fc5e7f9d1ff9a397131036d7fe8179
Ben Thomas <2719284+comnam90@users.noreply.github.com>
2026-01-06 18:01:35 +1300

    fix(testing): update mobile device configurations in Playwright setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,12 +31,12 @@ export default defineConfig({
 
     {
       name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
+      use: { ...devices['Pixel 7'] },
     },
 
     {
       name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
+      use: { ...devices['iPhone 15 Pro'] },
     },
   ],
 
